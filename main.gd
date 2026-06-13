fee8009d7025f8c4dc5b51c66fce5270edcf3f82
chickwithalmg-collab <chickwithalmg@gmail.com>
2026-06-13 21:57:14 +0200

    Added disclaimers and a first demo slide
    Added the first demo slide and a few disclaimers

--- main.gd
+++ main.gd
@@ -1,10 +1,16 @@
 extends TextureRect
 
+@export var Disclaimer: Label
+@export var TestDemo: Label
+@export var Film: VideoStreamPlayer
+
 func _on_next_slide_button_down() -> void:
 	pass # Replace with function body.
 
 func _on_of_legal_age_button_down() -> void:
-	pass # Replace with function body.
+	Disclaimer.visible = false
+	TestDemo.visible = true
 
-func _on_of_legal_age_2_button_down() -> void:
-	pass # Replace with function body.
+func _on_not_of_legal_age_button_down() -> void:
+	Film.visible = true
+	Film.play()
